6e50a7270a4f9ce53240857d58701c2741d293bf
Fortune Iheanacho <nachodev369@gmail.com>
2024-03-29 20:42:29 +0100

    Task 1. pint

--- opcode_one.c
+++ opcode_one.c
@@ -72,7 +72,7 @@ void pint(stack_t **stack, unsigned int line_num)
 	stack_t *temp;
 	unsigned int i;
 
-	if (stack == NULL && *stack == NULL)
+	if (stack == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_num);
 		exit(EXIT_FAILURE);
