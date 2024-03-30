c12c151fda3582eba3ff05135d84ab4c4b3208d5
Fortune Iheanacho <nachodev369@gmail.com>
2024-03-30 02:38:20 +0100

    Task 5. nop Edit

--- opcode_two.c
+++ opcode_two.c
@@ -80,7 +80,7 @@ void sub_opcode(stack_t **stack, unsigned int line_num)
 
 	if (!(*stack) || !(*stack)->next)
 	{
-		fprintf(stderr, "L%d: can't div, stack too short\n", line_num);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_num);
 		exit(EXIT_FAILURE);
 	}
 
